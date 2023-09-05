6383fcea80e6a54d3e39ae203c3f03a189c699db
Kevin Mars <kevin.eth.420@gmail.com>
2023-09-05 12:14:13 +0800

    to the top button completed

--- src/components/ToTheTopButton/ToTheTopButton.js
+++ src/components/ToTheTopButton/ToTheTopButton.js
@@ -1,16 +1,31 @@
 "use client"
 
+import { useState, useEffect } from 'react'
 import './tothetopbutton.css'
 import { AiOutlineArrowUp } from 'react-icons/ai'
 
 const ToTheTopButton = () => {
+    const [visible, setVisible] = useState(false)
 
-    function tothetop(){
-        window.scrollTo(0,0)
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 600) {
+                setVisible(true);
+            } else {
+                setVisible(false);
+            }
+        });
+    }, []);
+
+    function backtothetop() {
+        window.scrollTo(0, 0)
     }
 
     return (
-        <button className='tothetopbutton shadow' onClick={tothetop}>
+        <button
+            className={`tothetopbutton shadow ${visible ? '' : 'tothetopbutton_hide'}`}
+            onClick={backtothetop}
+        >
             <i className='top__arrow__icon'>
                 <AiOutlineArrowUp />
             </i>
